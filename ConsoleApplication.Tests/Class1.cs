c710e4553db73ea587ee21f719a5268860a45cfa
Дмитро Гавриляк <dmytro.havryliak@gmail.com>
2017-03-31 14:30:08 +0300

    practise

---
+++ ConsoleApplication.Tests/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ConsoleApplication1;
+using FluentAssert;
+using Moq;
+
+
+namespace ConsoleApplication.Tests
+{
+    [TestFixture]
+    public class NotifierTests
+    {
+        [Test]
+        public void Email_contests_should_be_ok()
+        {
+            var senderMock = new Mock<IEmailSender>();
+            senderMock.Setup(it)
+            var notifier = 
+        }
+    }
+
+    [TestFixture]
+    public class WarehouseTests
+    {
+        [Test]
+        public void When_Name_Does_Not_Contain_Food_Then_Storage_should_save_item()
+        {
+            var warehouse = new Warehouse();
+
+            warehouse.Add("Book");
+
+            warehouse.Count().ShouldBeEqualTo<uint>(1); 
+        }
+
+        [Test]
+        public void When_Name_Contains_Food_Then_Storage_should_throw_item()
+        {
+            var warehouse = new Warehouse();
+
+            Assert.Throws<InvalidOperationException>(() => new Warehouse().Add("Food"));
+        }
+
+        [Test]
+        public void When_try_to_remove_non_existent_item_should_throw()
+        {
+            var warehouse = new Warehouse(new string[] { "Spoon" });
+
+            warehouse.Count().ShouldBeEqualTo<uint>(1);
+
+            warehouse.Remove("Spoon");
+
+            warehouse.Count().ShouldBeEqualTo<uint>(0); 
+
+        }
+    }
+}
---
+++ ConsoleApplication1/Warehouse.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public interface IEmailSender
+    {
+        void SendEmail(string message, string email);
+    }
+
+    public class EmailSender : IEmailSender
+    {
+        public void SendEmail()
+        {
+
+        }
+
+        public void SendEmail(string message, string email)
+        {
+
+        }
+    }
+
+    public class EmailSenderStub : IEmailSender
+    {
+        public void SendEmail()
+        {
+
+        }
+
+        public void SendEmail(string message, string email)
+        {
+
+        }
+    }
+
+    public class EmailSenderFake : IEmailSender
+    {
+        public void SendEmail()
+        {
+
+        }
+
+        public void SendEmail(string message, string email)
+        {
+            File.WriteAllText("D:\\emails.txt", $"From:{email}\nMessage:{message}");
+        }
+    }
+
+    public class Notifier
+    {
+        private readonly IEmailSender sender;
+        public Notifier(IEmailSender sender)
+        {
+            this.sender = sender;
+        }
+
+        public void SendMessage(string message)
+        {
+            sender.SendEmail(message, "param@example.com");
+        }
+    }
+
+
+    public class Warehouse
+    {
+
+        Notifier notifier;
+
+        public Warehouse(Notifier notifier)
+        {
+            this.notifier = notifier;
+        }
+
+        public Warehouse(params string[] items)
+        {
+            if (items.Length < 1)
+            {
+                throw new InvalidOperationException();
+            }
+
+            items.ToList().ForEach(item => this.items.Add(item));
+        }
+        private HashSet<string> items = new HashSet<string>();
+
+        public void Add(string name)
+        {
+            if (name.Contains("Food"))
+                throw new InvalidOperationException($"cannot save item with name {name}");
+            items.Add(name);
+
+
+        }
+
+        public uint Count()
+        {
+            return (uint)items.Count;
+        }
+
+        public void Remove(string itemName)
+        {
+            if (!items.Contains(itemName))
+            {
+                throw new InvalidOperationException();
+            }
+
+            items.Remove(itemName);
+        }
+    }
+}
